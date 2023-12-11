42a81f3f93c12f6961e3a7e4c35db340c63077f0
Alexandre96dev <alexandre.nascimento96dev@gmail.com>
2023-12-10 23:57:36 -0300

    Adicionando o etl

--- etl.py
+++ etl.py
@@ -1,9 +1,49 @@
 import pandas as pd
-import sqlite3
-
+from database import Database
 class Etl:
-    def __init__(self):
-        self.dataframe = ''
     def etl(self):
-        dataframe = pd.read_parquet('databases/yellow_tripdata_2023-03.parquet')
-        print(dataframe)
+        meses = ['01', '02']
+
+        # dataframe_01 = pd.read_parquet('databases/yellow_tripdata_2023-01.parquet')
+        # dataframe_02 = pd.read_parquet('databases/yellow_tripdata_2023-02.parquet')
+        # dataframe_03 = pd.read_parquet('databases/yellow_tripdata_2023-03.parquet')
+        # dataframe_04 = pd.read_parquet('databases/yellow_tripdata_2023-04.parquet')
+        # dataframe_05 = pd.read_parquet('databases/yellow_tripdata_2023-05.parquet')
+        # dataframe_06 = pd.read_parquet('databases/yellow_tripdata_2023-06.parquet')
+        # dataframe_07 = pd.read_parquet('databases/yellow_tripdata_2023-07.parquet')
+        # dataframe_08 = pd.read_parquet('databases/yellow_tripdata_2023-08.parquet')
+        # dataframe_09 = pd.read_parquet('databases/yellow_tripdata_2023-09.parquet')
+
+        # dataframe = pd.concat([dataframe_01, dataframe_02, dataframe_03, dataframe_04, dataframe_05, dataframe_06, dataframe_07, dataframe_08, dataframe_09])
+
+        df = pd.read_parquet('databases/yellow_tripdata_2023-01.parquet')
+
+        df = df.dropna(subset=['tpep_pickup_datetime', 'tpep_dropoff_datetime', 'passenger_count'])
+
+        # Corrigindo erros de digitação ou formatação nas colunas
+        df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+        df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+
+        # Normalizando os valores das colunas para garantir que estejam no formato correto
+        df['passenger_count'] = df['passenger_count'].astype('int')
+        del df['VendorID']
+        del df['store_and_fwd_flag']
+        
+        # ETL
+
+        # Calculando a duração da viagem
+        df['duracao_viagem'] = (df['tpep_dropoff_datetime'] - df['tpep_pickup_datetime']).dt.total_seconds() / 3600
+
+        df['trip_distance'] = df['trip_distance'] * 1609.344
+
+        # Calculando a tarifa final
+        df['tarifa_final'] = df['total_amount'] + df['extra'] + df['mta_tax'] + df['improvement_surcharge'] + df['tip_amount'] + df['tolls_amount'] + df['congestion_surcharge'] + df['airport_fee']
+        
+        dim_taxi = pd.read_csv('databases/taxi+_zone_lookup.csv')
+        
+        
+        
+        
+
+t = Etl()
+t.etl()
--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-from etl import Etl
+import pandas as pd
 
-etlObj = Etl()
+schema = {
+    'dim_ratecode': ['RatecodeID'],
+    'dim_payment_type': ['payment_type'],
+    'fact_vendas': ['id_venda', 'id_cliente', 'id_produto', 'quantidade', 'valor']
+}
 
-etlObj.etl()
+dataframe_ = pd.read_parquet('databases/yellow_tripdata_2023-03.parquet')
+
+
+dim_ratecode = dataframe_[schema['dim_ratecode']]
+
+dim_ratecode = dim_ratecode.loc[dim_ratecode['RatecodeID'].isin([1.0, 2.0, 3.0, 4.0, 5.0, 6.0])]
+
+dim_ratecode = dim_ratecode.drop_duplicates()
+
+
+
+dim_payment_type = dataframe_[schema['dim_payment_type']]
+
+dim_payment_type = dim_payment_type.loc[dim_payment_type['payment_type'].isin([1, 2, 3, 4, 5])]
+
+dim_payment_type = dim_payment_type.drop_duplicates()
+
+
+print(dim_payment_type)
